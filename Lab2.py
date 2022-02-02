9ac5c24eb3001c4a087e2482e5c8012aa368cee7
EC2 Default User <ec2-user@ip-172-31-88-63.ec2.internal>
2022-02-02 03:10:42 +0000

    lab2

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,47 @@
+'''
+lab2.py
+'''
 
+#3.1
+
+my_name = "tom"
+print(my_name.upper())
+
+#3.2
+
+my_id =123
+print(my_id)
+
+#3.3
+#123=my_id
+#Does not work, must start right to left
+
+my_id=your_id=123
+print(my_id)
+print(your_id)
+
+#3.4
+
+my_id_str= "123"
+print(my_id_str)
+
+#3.5
+#print(my_name+my_id)
+# no. can not use string and integer together
+
+#3.6
+
+print(my_name+my_id_str)
+
+#3.7
+
+print(my_name*3)
+
+#3.8
+
+print("Hello, world. This is my first python string.".split(".") )
+
+#3.8
+
+#message= 'tom's id is 123'
+#print(message)
